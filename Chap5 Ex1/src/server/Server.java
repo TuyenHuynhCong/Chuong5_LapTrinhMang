0a0c87cf1cb218e466f3ae78166c8520847ec4f7
TuyenHuynhCong <huynhcongtuyen1998@gmail.com>
2019-12-03 19:54:33 +0700

    update chap5

---
+++ Chap5 Ex1/src/server/Server.java
@@ -0,0 +1,24 @@
+package server;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Server {
+	private static final int PORT = 123;
+	   private static Registry registry;
+	public static void main(String[] args) {
+		   try {
+			   //Tao may chu rmi
+			registry =  LocateRegistry.createRegistry(PORT);
+			//dang ki phuong thuc
+			registry.bind("my RMI", new TaskImplement());
+			System.out.println("Server started! Task ready for call!");
+			
+		} catch (Exception e) {
+			// 
+			e.printStackTrace();
+		}
+		   
+	}
+
+}
